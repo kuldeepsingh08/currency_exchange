f476a702d5442f017e0542fbd7c404d25c9f08ac
kuldeeps1ngh <ksingh.singh060@gmail.com>
2022-03-24 21:45:47 +0530

    added header

--- src/app/exchanger/exchanger.module.ts
+++ src/app/exchanger/exchanger.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ExchangerRoutingModule } from './exchanger-routing.module';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+import { PanelComponent } from './panel/panel.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    DetailsComponent
+    DetailsComponent,
+    PanelComponent
   ],
   imports: [
     CommonModule,
